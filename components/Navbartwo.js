3edf0064bb553b8e0ed2a8179dd142175f926017
Sameer Patwari <sameerpatwari2003@gmail.com>
2025-05-31 23:01:31 +0530

    Create Navbartwo.js

---
+++ components/Navbartwo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbartwo = () => {
+  return (
+    <div className='flex justify-center gap-8 items-center'>
+        <h4 className='text-xl'>Home</h4>
+        <h4 className='text-xl'>About</h4>
+        <h4 className='text-xl'>Contact</h4>
+        <h4 className='text-xl'>Login</h4>
+    </div>
+  )
+}
+
+export default Navbartwo
