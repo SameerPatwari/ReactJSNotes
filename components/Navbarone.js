21b099f726b673377a33a290b29229729ae54d0d
Sameer Patwari <sameerpatwari2003@gmail.com>
2025-05-31 23:00:51 +0530

    Create Navbarone.js

---
+++ components/Navbarone.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbarone = () => {
+  return (
+    <div className='flex px-10 py-5 justify-center items-center bg-gray-800 text-white'>
+       <h2 className='text-2xl font-bold'>Sameer's App</h2>
+    </div>
+  )
+}
+
+export default Navbarone
