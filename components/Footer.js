7386e4d1944565e19837e8e009b7abc08e3790fc
Sameer Patwari <sameerpatwari2003@gmail.com>
2025-05-31 23:02:05 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='text-2xl font-semibold flex px-10 py-5 justify-center items-center bg-gray-800 text-white absolute bottom-0 w-full'>
+      <h1>THIS IS A FOOTER</h1>
+    </div>
+  )
+}
+
+export default Footer
