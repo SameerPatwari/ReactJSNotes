87d08109af6a0f19eeb94fcbc8218d4884c6c3ed
Sameer Patwari <sameerpatwari2003@gmail.com>
2025-05-31 22:59:59 +0530

    Create App.js

---
+++ components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import Navbartwo from './components/Navbartwo.jsx'
+import Navbarone from './components/Navbarone.jsx'
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Navbarone />
+      <Navbartwo />
+      <Footer />
+    </>
+  )
+}
+
+export default App
