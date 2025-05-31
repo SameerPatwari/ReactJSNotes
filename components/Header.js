28ef787eb88bc9be07b8390357db49f659c2f46b
Sameer Patwari <sameerpatwari2003@gmail.com>
2025-05-31 23:00:27 +0530

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className=' text-4xl font-semibold flex px-10 py-5 justify-center items-center bg-gray-800 text-white'>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default Header
